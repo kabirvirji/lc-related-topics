885e7690ed9d8ed602415547fa917eeebcbb7fb2
kabirvirji <>
2020-12-28 19:52:04 -0800

    mvp functionality

--- popup.js
+++ popup.js
@@ -1,10 +1,40 @@
+var topics = null
 chrome.tabs.executeScript(null, {file: './foreground.js'}, () => {
     console.log('injected')
     chrome.storage.local.get(['topics'], function(result) {
         console.log('Value currently is ' + result.topics[1]);
-        var topics = result.topics
+        topics = result.topics
+        console.log(topics)
     });
 })
+
+document.getElementById('submit').onclick = function check() {
+    
+    var checked_topics = []
+    var correct = 1
+    var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+    for (var i = 0; i < checkboxes.length; i++) {
+      checked_topics.push(checkboxes[i].value)
+    }
+    console.log(checked_topics, topics)
+    if (checked_topics.length != topics.length) {
+        alert(`${checked_topics.length},${topics.length}`)
+    } else {
+        // check all elements
+        for (var j = 0; j < checked_topics.length; j++) {
+            if (!topics.includes(checked_topics[j])) {
+                alert(false)
+                correct = 0
+                break
+            }
+        }
+    }
+    if (correct == 1) {
+        alert(true)
+    }
+    
+ 
+ }
 
 // const check_answers = (topics) => {
 // }
